0fba7a611e271cd7bf1e30806d3db8c614dccb7e
u <anqil0660@gmail.com>
2026-07-05 20:20:29 +0800

    first commit

---
+++ src/utils/score.js
@@ -0,0 +1,40 @@
+﻿export function createEmptyScores(dimensions) {
+  return Object.fromEntries(dimensions.map((dimension) => [dimension, 0]));
+}
+
+export function calculateScores({ questions, answers, dimensions }) {
+  const scores = createEmptyScores(dimensions);
+
+  questions.forEach((question) => {
+    const selectedKey = answers[String(question.id)];
+    const selectedOption = question.options.find((option) => option.key === selectedKey);
+
+    if (!selectedOption) {
+      return;
+    }
+
+    Object.entries(selectedOption.scores).forEach(([dimension, value]) => {
+      if (Object.prototype.hasOwnProperty.call(scores, dimension)) {
+        scores[dimension] += value;
+      }
+    });
+  });
+
+  return scores;
+}
+
+export function pickPrimaryAndSecondary(scores, dimensions) {
+  const ranked = dimensions
+    .map((dimension, order) => ({ dimension, order, score: scores[dimension] ?? 0 }))
+    .sort((a, b) => b.score - a.score || a.order - b.order);
+
+  return {
+    primary: ranked[0]?.dimension || "",
+    secondary: ranked[1]?.dimension || "",
+    ranked
+  };
+}
+
+export function isComplete(questions, answers) {
+  return questions.every((question) => Boolean(answers[String(question.id)]));
+}
