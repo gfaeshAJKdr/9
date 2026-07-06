3a05c1a4a4e81c95ba16beb1b44bb91811120727
gfaeshAJKdr <3349619410@qq.com>
2026-07-06 10:40:06 +0800

    Delete 打包份 directory

--- 打包份/src/utils/data.js
+++
@@ -1,9 +0,0 @@
-﻿export async function loadJson(path) {
-  const response = await fetch(path, { cache: "no-store" });
-
-  if (!response.ok) {
-    throw new Error(`数据读取失败：${path}`);
-  }
-
-  return response.json();
-}
